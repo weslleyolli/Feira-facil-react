1ccceae903d7595cf698b9888bcd6aa17a3417f1
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-05-23 13:43:55 -0300

    starting project

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import { Roboto_Flex as Roboto } from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto'})
+
+export const metadata = {
+  title: 'Feira Facil',
+  description: 'Uma forma descomplicada de ir a feira sem sair de casa!',
+}
+
+export default function RootLayout({children,}: {children: ReactNode}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello world</h1>
+    </div>
+  )
+}
