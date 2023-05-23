774e0581560b477cd77993d5f2673477dc8a087e
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-05-23 14:00:57 -0300

    login

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,52 @@
+import Image from 'next/image'
+import feiraLogo from '../assets/logoIcon.svg'
+import ballOne from '../assets/ballOne.svg'
+import ballTwo from '../assets/ballTwo.svg'
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-500">Hello world</h1>
+      <div>
+        <Image src={feiraLogo} alt="Logo"/>
+    </div>
+    <div >
+        <Image src={ballOne} alt="Bola verde"/>
+    </div>
+    <div >
+        <div>
+            <p>Login</p>
+        </div>
+        <div>
+            <div>
+                <p>Email</p>
+                <input id="username" type="email" placeholder="email@email.com"/>
+            </div>
+            <div>
+                <div>
+                    <input id="password" type="password" placeholder="***************"/>
+                </div>
+                <div>
+                    <button id="forget"> <a href=""> Esqueceu a senha? </a></button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <button id="enter">
+                <a href="">
+                    <p>Entrar</p>
+                </a>
+            </button>
+
+            <button id="register">
+                <a href="">
+                    <p>Cadastre-se</p>
+                </a>
+            </button>
+        </div>
+    </div>
+    <div>
+        <Image src={ballTwo} alt="Bola verde"/>
+    </div>
     </div>
   )
 }
